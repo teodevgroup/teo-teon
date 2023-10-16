37d95cc5ce3dd88e559bcc91586f869341a1c794
XiaYuan Wu <i2256g03c3@163.com>
2023-10-16 23:40:30 +0800

    ref to ref

--- src/convert/into/decimal.rs
+++ src/convert/into/decimal.rs
@@ -26,6 +26,18 @@ impl TryFrom<Value> for BigDecimal {
     }
 }
 
+impl<'a> TryFrom<&'a Value> for &'a BigDecimal {
+
+    type Error = Error;
+
+    fn try_from(value: &'a Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Decimal(s) => Ok(s),
+            _ => Err(Error::new(format!("Cannot convert {} into &BigDecimal", value.type_hint()))),
+        }
+    }
+}
+
 impl TryFrom<Value> for Option<BigDecimal> {
 
     type Error = Error;
@@ -48,6 +60,19 @@ impl TryFrom<&Value> for Option<BigDecimal> {
             Value::Null => Ok(None),
             Value::Decimal(b) => Ok(Some(b.clone())),
             _ => Err(Error::new(format!("Cannot convert {} into Option<BigDecimal>", value.type_hint()))),
+        }
+    }
+}
+
+impl<'a> TryFrom<&'a Value> for Option<&'a BigDecimal> {
+
+    type Error = Error;
+
+    fn try_from(value: &'a Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Null => Ok(None),
+            Value::Decimal(d) => Ok(Some(d)),
+            _ => Err(Error::new(format!("Cannot convert {} into Option<&BigDecimal>", value.type_hint()))),
         }
     }
 }
--- src/convert/into/string.rs
+++ src/convert/into/string.rs
@@ -1,14 +1,14 @@
 use crate::error::Error;
 use crate::value::Value;
 
-impl TryInto<String> for Value {
+impl TryFrom<Value> for String {
 
     type Error = Error;
 
-    fn try_into(self) -> Result<String, Self::Error> {
-        match self {
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
             Value::String(s) => Ok(s),
-            _ => Err(Error::new(format!("Cannot convert {} into String", self.type_hint()))),
+            _ => Err(Error::new(format!("Cannot convert {} into String", value.type_hint()))),
         }
     }
 }
@@ -25,24 +25,53 @@ impl TryFrom<&Value> for String {
     }
 }
 
-impl TryInto<Option<String>> for Value {
+impl<'a> TryFrom<&'a Value> for &'a String {
 
     type Error = Error;
 
-    fn try_into(self) -> Result<Option<String>, Self::Error> {
-        match self {
-            Value::Null => Ok(None),
-            Value::String(s) => Ok(Some(s)),
-            _ => Err(Error::new(format!("Cannot convert {} into Option<String>", self.type_hint()))),
+    fn try_from(value: &'a Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::String(s) => Ok(s),
+            _ => Err(Error::new(format!("Cannot convert {} into &String", value.type_hint()))),
         }
     }
 }
 
-impl TryInto<Option<String>> for &Value {
+impl TryFrom<Value> for Option<String> {
 
     type Error = Error;
 
-    fn try_into(self) -> Result<Option<String>, Self::Error> {
-        self.clone().try_into()
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Null => Ok(None),
+            Value::String(s) => Ok(Some(s)),
+            _ => Err(Error::new(format!("Cannot convert {} into Option<String>", value.type_hint()))),
+        }
+    }
+}
+
+impl TryFrom<&Value> for Option<String> {
+
+    type Error = Error;
+
+    fn try_from(value: &Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Null => Ok(None),
+            Value::String(s) => value.clone().try_into(),
+            _ => Err(Error::new(format!("Cannot convert {} into Option<String>", value.type_hint()))),
+        }
+    }
+}
+
+impl<'a> TryFrom<&'a Value> for Option<&'a String> {
+
+    type Error = Error;
+
+    fn try_from(value: &'a Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Null => Ok(None),
+            Value::String(s) => Ok(Some(s)),
+            _ => Err(Error::new(format!("Cannot convert {} into Option<&String>", value.type_hint()))),
+        }
     }
 }
