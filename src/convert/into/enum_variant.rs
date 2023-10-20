5dd6d9c62d05ffe52d02b718a5fa258e9c5582e6
Victor Teo <victor.teo@fillmula.com>
2023-10-21 04:17:59 +0800

    enum

--- src/convert/into/enum_variant.rs
+++ src/convert/into/enum_variant.rs
@@ -2,52 +2,64 @@ use teo_result::Error;
 use crate::types::enum_variant::EnumVariant;
 use crate::value::Value;
 
-impl TryInto<EnumVariant> for Value {
+impl TryFrom<Value> for EnumVariant {
 
     type Error = Error;
 
-    fn try_into(self) -> Result<EnumVariant, Self::Error> {
-        match self {
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
             Value::EnumVariant(s) => Ok(s),
-            _ => Err(Error::new(format!("Cannot convert {} into EnumVariant", self.type_hint()))),
+            _ => Err(Error::new(format!("Cannot convert {} into EnumVariant", value.type_hint()))),
         }
     }
 }
 
-impl<'a> TryInto<&'a EnumVariant> for &'a Value {
+impl<'a> TryFrom<&'a Value> for EnumVariant {
 
     type Error = Error;
 
-    fn try_into(self) -> Result<&'a EnumVariant, Self::Error> {
-        match self {
-            Value::EnumVariant(s) => Ok(s),
-            _ => Err(Error::new(format!("Cannot convert {} into &EnumVariant", self.type_hint()))),
+    fn try_from(value: &'a Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::EnumVariant(s) => Ok(s.clone()),
+            _ => Err(Error::new(format!("Cannot convert {} into EnumVariant", value.type_hint()))),
         }
     }
 }
 
-impl TryInto<Option<EnumVariant>> for Value {
+impl<'a> TryFrom<&'a Value> for &'a EnumVariant {
 
     type Error = Error;
 
-    fn try_into(self) -> Result<Option<EnumVariant>, Self::Error> {
-        match self {
+    fn try_from(value: &'a Value) -> Result<&'a EnumVariant, Self::Error> {
+        match value {
+            Value::EnumVariant(s) => Ok(s),
+            _ => Err(Error::new(format!("Cannot convert {} into &EnumVariant", value.type_hint()))),
+        }
+    }
+}
+
+impl TryFrom<Value> for Option<EnumVariant> {
+
+    type Error = Error;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
             Value::Null => Ok(None),
             Value::EnumVariant(s) => Ok(Some(s)),
-            _ => Err(Error::new(format!("Cannot convert {} into Option<EnumVariant>", self.type_hint()))),
+            _ => Err(Error::new(format!("Cannot convert {} into Option<EnumVariant>", value.type_hint()))),
         }
     }
 }
 
-impl<'a> TryInto<Option<&'a EnumVariant>> for &'a Value {
+impl<'a> TryFrom<&'a Value> for Option<&'a EnumVariant> {
 
     type Error = Error;
 
-    fn try_into(self) -> Result<Option<&'a EnumVariant>, Self::Error> {
-        match self {
+    fn try_from(value: &'a Value) -> Result<Option<&'a EnumVariant>, Self::Error> {
+        match value {
             Value::Null => Ok(None),
             Value::EnumVariant(s) => Ok(Some(s)),
-            _ => Err(Error::new(format!("Cannot convert {} into Option<&EnumVariant>", self.type_hint()))),
+            _ => Err(Error::new(format!("Cannot convert {} into Option<&EnumVariant>", value.type_hint()))),
         }
     }
 }
